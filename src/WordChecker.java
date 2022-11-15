3fb72a4cf6decd53c0f688c27723777ee2ad6a92
Christina Kungurtseva <kungurts@gmail.com>
2022-11-15 12:57:33 +0300

    initial commit

---
+++ src/WordChecker.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class WordChecker {
+    String text;
+
+    public WordChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean hasWord(String word) {
+        Set<String> words = new HashSet<>();
+        String[] wordsArray = text.split("\\P{IsAlphabetic}+");
+        for (String w : wordsArray) {
+            String wLowerCase = w.toLowerCase();
+            words.add(wLowerCase);
+        }
+        String wordLowerCase = word.toLowerCase();
+        if (words.contains(wordLowerCase)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
